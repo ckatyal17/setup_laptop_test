7d9522dc3e3daf6d3dce1988a41f23289f78a34b
ckatyal <chanakyakatyal@hotmail.com>
2024-04-14 09:51:38 +0000

    Updated PowerShell script

--- PowerShell_Scripts/configureBrowser.ps1
+++ PowerShell_Scripts/configureBrowser.ps1
@@ -1,84 +1,106 @@
-﻿$installDir = "C:\Amzn-New-Win-Setup\BrowserSetup"
-
-New-Item -ItemType "directory" -Path $installDir
-
-## Check if Firefox is Installed - If not then install firefox else do nothing:
-
-$x86_check = ((Get-ChildItem "HKLM:Software\Microsoft\Windows\CurrentVersion\Uninstall") | Where-Object { $_."Name" -like "*Mozilla Firefox*" } ).Length -gt 0
-$x64_check = ((Get-ChildItem "HKLM:Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall") | Where-Object { $_."Name" -like "*Mozilla*" } ).Length -gt 0
-$mozilla = (Get-CimInstance -ClassName CCM_Application -Namespace "root\ccm\clientSDK" | Where-Object {$_.Name -eq 'Mozilla Firefox ESR'})
-$mozilla32 = (Get-CimInstance -ClassName CCM_Application -Namespace "root\ccm\clientSDK" | Where-Object {$_.Name -eq 'Mozilla Firefox ESR 32-bit'})
-
-if (($x86_check -eq $true) -or ($x64_check -eq $true)) {
-    if(($mozilla32.InstallState -eq 'Installed') -or ($mozilla.InstallState -eq 'Installed')){
-            Write-Host "Firefox is installed using Software Center"
-    }else{
-        Write-Host "Firefox is installed but not using Software center" 
-    } 
-}else {
-    Write-Host "##################################`nInstalling Mozilla firefox ESR`n##################################"
-    $firefox = (Get-CimInstance -ClassName CCM_Application -Namespace "root\ccm\clientSDK" | Where-Object {$_.Name -eq 'Mozilla Firefox ESR 32-bit'})
-    $Args = @{EnforcePreference = [UINT32] 0
-    Id = "$($firefox.id)"
-    IsMachineTarget = $firefox.IsMachineTarget
-    IsRebootIfNeeded = $False
-    Priority = 'High'
-    Revision = "$($firefox.Revision)" }
-
-    $output = Invoke-CimMethod -Namespace "root\ccm\clientSDK" -ClassName CCM_Application -MethodName Install -Arguments $Args
-    if ($output.ReturnValue -eq 0){
-        Write-Host "Mozilla Firefox installed successfully"
-    }else {$output}
+﻿# Create folder
+$installDir = "C:\Amzn-New-Win-Setup\BrowserSetup"
+if (-not (Test-Path $installDir -PathType Container)) {
+    Write-Host "Creating folder to store browser configuration files..."
+    try {
+        New-Item -ItemType Directory -Path $installDir -Force | Out-Null
+    } catch {
+        Write-Host "Failed to create folder: $_"
+        return
+    }
 }
 
-########################################################
+# Check if Firefox is installed
+$firefoxInstalled = $false
+$firefoxInstances = Get-CimInstance -ClassName CCM_Application -Namespace "root\ccm\clientSDK" | Where-Object { $_.Name -like '*Mozilla Firefox*' }
+if ($firefoxInstances) {
+    $firefoxInstalled = $true
+}
+
+if (-not $firefoxInstalled) {
+    Write-Host "Installing Mozilla Firefox ESR..."
+    $firefox = Get-CimInstance -ClassName CCM_Application -Namespace "root\ccm\clientSDK" | Where-Object { $_.Name -like 'Mozilla Firefox ESR 32-bit' }
+    $Args = @{
+        EnforcePreference = 0
+        Id = $firefox.id
+        IsMachineTarget = $firefox.IsMachineTarget
+        IsRebootIfNeeded = $false
+        Priority = 'High'
+        Revision = $firefox.Revision
+    }
+
+    try {
+        $output = Invoke-CimMethod -Namespace "root\ccm\clientSDK" -ClassName CCM_Application -MethodName Install -Arguments $Args
+        if ($output.ReturnValue -eq 0) {
+            Write-Host "Mozilla Firefox installed successfully"
+        } else {
+            Write-Host "Failed to install Mozilla Firefox: $($output.ReturnValue)"
+            return
+        }
+    } catch {
+        Write-Host "Failed to install Mozilla Firefox: $_"
+        return
+    }
+}
+
 # Download Certificates
+$certificates = @(
+    "https://pki.amazon.com/crt/Amazon.com%20Internal%20Root%20Certificate%20Authority.der",
+    "https://pki.amazon.com/crt/Amazon.com%20CIA%20CA%20G5%2001.der",
+    "https://pki.amazon.com/crt/Amazon.com%20CIA%20CA%20G5%2002.der"
+)
 
-Invoke-WebRequest -Uri "https://pki.amazon.com/crt/Amazon.com%20Internal%20Root%20Certificate%20Authority.der" -OutFile "$($installDir)\Amazon.com Internal Root Certificate Authority.der"
-Invoke-WebRequest -Uri "https://pki.amazon.com/crt/Amazon.com%20CIA%20CA%20G5%2001.der" -OutFile "$($installDir)\Amazon.com CIA CA G5 01.der"
-Invoke-WebRequest -Uri "https://pki.amazon.com/crt/Amazon.com%20CIA%20CA%20G5%2002.der" -OutFile "$($installDir)\Amazon.com CIA CA G5 02.der"
+foreach ($certUrl in $certificates) {
+    $certName = $certUrl.Split('/')[-1]
+    $certPath = Join-Path -Path $installDir -ChildPath $certName
+    if (-not (Test-Path $certPath)) {
+        Write-Host "Downloading certificate: $certName"
+        try {
+            Invoke-WebRequest -Uri $certUrl -OutFile $certPath -ErrorAction Stop
+        } catch {
+            Write-Host "Failed to download certificate: $_"
+        }
+    } else {
+        Write-Host "Certificate already exists: $certName"
+    }
+}
 
-########################################################
-# Install Tampermonkey
+# Install Tampermonkey extension
 $registryPath = "HKLM:\SOFTWARE\Policies\Mozilla\Firefox\Extensions\Install"
+$tampermonkeyUrl = "https://addons.mozilla.org/firefox/downloads/file/3768983/tampermonkey-4.13.6136-an+fx.xpi"
 
-$Name = "100"
+if (-not (Test-Path $registryPath)) {
+    Write-Host "Tampermonkey registry path not found"
+    return
+}
 
-$Value = "https://addons.mozilla.org/firefox/downloads/file/3768983/tampermonkey-4.13.6136-an+fx.xpi"
+if (-not (Get-ItemProperty -Path $registryPath -Name 100 -ErrorAction SilentlyContinue)) {
+    Write-Host "Installing Tampermonkey extension..."
+    try {
+        New-ItemProperty -Path $registryPath -Name 100 -Value $tampermonkeyUrl -PropertyType String -Force | Out-Null
+    } catch {
+        Write-Host "Failed to install Tampermonkey extension: $_"
+    }
+} else {
+    Write-Host "Tampermonkey extension already installed"
+}
 
-New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType String -Force | Out-Null
+# Install Certificates
+if (Test-Path $installDir -PathType Container) {
+    Write-Host "Installing certificates..."
+    $certRegistryPath = "HKLM:\SOFTWARE\Policies\Mozilla\Firefox\Certificates\Install"
+    $certName = 100
+    $certFiles = Get-ChildItem -Path $installDir -File
 
-########################################################
-Install Certificates
-$certRegistryPath = "HKLM:\SOFTWARE\Policies\Mozilla\Firefox\Certificates\Install"
-
-$certName = "100"
-
-$cert = "$($installDir)\Amazon.com Internal Root Certificate Authority.der"
-
-$certName1 = "101"
-
-$cert1 = "$($installDir)\Amazon.com InfoSec CA G3.der"
-
-$certName2 = "102"
-
-$cert2 = "$($installDir)\Amazon.com InfoSec CA G4 ACM1.der"
-
-$certName3 = "103"
-
-$cert3 = "$($installDir)\Amazon.com InfoSec CA G4 ACM2.der"
-
-$certName4 = "104"
-
-$cert4 = "$($installDir)\CIA-CRT-G3-01.ant.amazon.com_Amazon.com CIA CA G3 01.der"
-
-$certName5 = "105"
-
-$cert5 = "$($installDir)\CIA-CRT-G3-02.ant.amazon.com_Amazon.com CIA CA G3 02.der"
-
-New-ItemProperty -Path $certRegistryPath -Name $certName -Value $cert -PropertyType String -Force | Out-Null
-New-ItemProperty -Path $certRegistryPath -Name $certName1 -Value $cert1 -PropertyType String -Force | Out-Null
-New-ItemProperty -Path $certRegistryPath -Name $certName2 -Value $cert2 -PropertyType String -Force | Out-Null
-New-ItemProperty -Path $certRegistryPath -Name $certName3 -Value $cert3 -PropertyType String -Force | Out-Null
-New-ItemProperty -Path $certRegistryPath -Name $certName4 -Value $cert4 -PropertyType String -Force | Out-Null
-New-ItemProperty -Path $certRegistryPath -Name $certName5 -Value $cert5 -PropertyType String -Force | Out-Null
+    foreach ($certFile in $certFiles) {
+        $certPath = $certFile.Name
+        try {
+            New-ItemProperty -Path $certRegistryPath -Name $certName -Value $certPath -PropertyType String -Force | Out-Null
+            $certName++
+        } catch {
+            Write-Host "Failed to install certificate $($certFile.Name): $_"
+        }
+    }
+} else {
+    Write-Host "Folder does not exist: $installDir"
+}
