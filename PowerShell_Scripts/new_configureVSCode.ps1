52993da01bd7866a82365daf8595c7dc4b7aeeca
ckatyal <chanakyakatyal@hotmail.com>
2024-04-14 21:59:09 +0000

    Updated to comm configuration PS.

---
+++ PowerShell_Scripts/new_configureVSCode.ps1
@@ -0,0 +1,37 @@
+# Function to install applications with error handling
+function Install-Application {
+    param (
+        [string]$AppName
+    )
+
+    try {
+        # Attempt to get application instance
+        $app = Get-CimInstance -ClassName CCM_Application -Namespace "root\ccm\clientSDK" | Where-Object { $_.Name -like $AppName }
+
+        # Check if application instance is retrieved
+        if ($app) {
+            $Args = @{
+                EnforcePreference = [UINT32]0
+                Id = "$($app.id)"
+                IsMachineTarget = $app.IsMachineTarget
+                IsRebootIfNeeded = $False
+                Priority = 'High'
+                Revision = "$($app.Revision)" 
+            }
+        
+            # Attempt to install application
+            Invoke-CimMethod -Namespace "root\ccm\clientSDK" -ClassName CCM_Application -MethodName Install -Arguments $Args
+            Write-Output "$AppName installed successfully."
+        } else {
+            Write-Output "Application instance not found for $AppName."
+        }
+    } catch {
+        Write-Output "Failed to install $AppName: $_"
+    }
+}
+
+# Install Chime
+Install-Application -AppName 'Amazon Chime'
+
+# Install Slack
+Install-Application -AppName 'Slack'
